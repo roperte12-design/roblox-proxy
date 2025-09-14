30efa022e577184ab25227669dd3ac883e0f94f0
roperte12-design <roperte12@gmail.com>
2025-09-14 17:26:58 +0200

    Update gamepasses.js

--- api/gamepasses.js
+++ api/gamepasses.js
@@ -1,18 +1,2 @@
-// api/gamepasses.js
-export default async function handler(req, res) {
-  const { userId } = req.query;
-
-  if (!userId) {
-    return res.status(400).json({ error: "Falta userId" });
-  }
-
-  try {
-    const response = await fetch(
-      `https://games.roblox.com/v2/users/${userId}/game-passes?limit=50`
-    );
-    const data = await response.json();
-    return res.status(200).json(data);
-  } catch (err) {
-    return res.status(500).json({ error: "Error consultando Roblox API" });
-  }
-}
+/api
+  └── gamepasses.js
